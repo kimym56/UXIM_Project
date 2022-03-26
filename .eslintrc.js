0315ca3ceedc61b60e66d4ca1f2534865873a696
kimym56 <74525390+kimym56@users.noreply.github.com>
2022-03-26 19:47:42 +0900

    Set Eslint & Add SafeAreaView with DarkMode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+	'env': {
+		'browser': true,
+    'es2021': true,
+    'node' : true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'2'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
